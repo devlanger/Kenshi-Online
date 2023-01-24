8bb483d70f6709831f06b441fa9b261967602b4a
Piotr Langer <piotr.langer@amsterdamstandard.com>
2023-01-24 18:50:53 +0100

    dead state

---
+++ KenshiClient/Kenshi/Assets/Scripts/States/CombatStates/DeadState.cs
@@ -0,0 +1,28 @@
+using Kenshi.Shared.Enums;
+using UnityEngine;
+
+namespace StarterAssets.CombatStates
+{
+    public class DeadState : FSMState
+    {
+        public override FSMStateId Id => FSMStateId.dead;
+
+        protected override void OnUpdate(PlayerStateMachine stateMachine)
+        {
+        }
+
+        protected override void OnInputUpdate(PlayerStateMachine stateMachine)
+        {
+        }
+
+        protected override void OnEnter(PlayerStateMachine stateMachine)
+        {
+            stateMachine.Target.animator.SetBool("dead", true);
+        }
+
+        protected override void OnExit(PlayerStateMachine stateMachine)
+        {
+            stateMachine.Target.animator.SetBool("dead", false);
+        }
+    }
+}
